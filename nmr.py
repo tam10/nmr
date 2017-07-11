3815695dfb643d4380bce6412519404b36bf8494
Tristan Mackenzie <tam10@dyn231-209.ch.ic.ac.uk>
2017-07-11 11:04:03 +0100

    License

--- nmr.py
+++ nmr.py
@@ -2,7 +2,18 @@
 """
 Created on Thu Jul  6 11:50:10 2017
 
-@author: tam10
+@author: Tristan Mackenzie
+
+    QNMR is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    QNMR is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with QNMR.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import numpy as np
